bc7f4522058b25c3b64e153f2c490cc78d3d74e8
Neil Massey <neil.massey@stfc.ac.uk>
2024-06-26 17:05:31 +0100

    Update setup.py - inc version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='nla_control',
-    version='1.2.1',
+    version='1.2.2',
     packages=['nla_control'],
     install_requires=[
         'appdirs',
